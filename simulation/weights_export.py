ce358bca9f2a7e9db9695f877e3eb254ae4215d7
Vastu-verma <vastu@iitg.ac.in>
2026-04-04 12:20:03 +0530

    Update weights_export.py

--- simulation/weights_export.py
+++ simulation/weights_export.py
@@ -6,6 +6,7 @@ quantized_weights = np.clip(np.round(weights * 127), -128, 127).astype(np.int8)
 with open("weights.vh", "w") as f:
     for class_idx in range(10):
         for pixel_idx in range(196):
-            val = quantized_weights[class_idx, pixel_idx]
+          
+            val = int(quantized_weights[class_idx, pixel_idx])
             hex_val = f"{(val & 0xFF):02X}\n"
             f.write(hex_val)
